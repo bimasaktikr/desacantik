7034233de57e8ffa4372af0d15089e14004ec20e
bimasaktikr <bimasakti.kr@gmail.com>
2025-06-25 08:56:48 +0700

    change from routes/console to bootstrap/app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,4 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        // ✅ Tambahkan perintah queue:work
+        $schedule->command('queue:work --once')->everyMinute();
+
+        // Tambahkan task lain jika perlu
+        // $schedule->command('your:other-command')->daily();
+    })
+    ->create();
