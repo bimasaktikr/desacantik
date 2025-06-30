0c9a207a033eab4f0887786dee787988a551a036
bimasaktikr <bimasakti.kr@gmail.com>
2025-06-30 12:07:25 +0700

    add tabs to list assignment uplaod

--- app/Filament/Resources/AssignmentUploadResource.php
+++ app/Filament/Resources/AssignmentUploadResource.php
@@ -13,6 +13,7 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Models\User;
+use Filament\Tables\Tab;
 
 class AssignmentUploadResource extends Resource
 {
--- app/Filament/Resources/AssignmentUploadResource/Pages/ListAssignmentUploads.php
+++ app/Filament/Resources/AssignmentUploadResource/Pages/ListAssignmentUploads.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\AssignmentUploadResource\Pages;
 use App\Filament\Resources\AssignmentUploadResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
 
 class ListAssignmentUploads extends ListRecords
 {
@@ -14,6 +15,19 @@ class ListAssignmentUploads extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All'),
+            'success' => Tab::make('Success')
+                ->modifyQueryUsing(fn ($query) => $query->where('import_status', 'berhasil')),
+            'failed' => Tab::make('Failed')
+                ->modifyQueryUsing(fn ($query) => $query->where('import_status', 'gagal')),
+            'pending' => Tab::make('Pending')
+                ->modifyQueryUsing(fn ($query) => $query->where('import_status', 'pending')),
         ];
     }
 }
